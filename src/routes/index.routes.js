ee7f3d3a346cab02f32de2bd11f2487e81f066fb
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2020-08-20 14:36:25 -0500

    Added routes

---
+++ src/routes/index.routes.js
@@ -0,0 +1,29 @@
+import Header from '../template/Header';
+import Home from '../page/Home';
+import Character from '../page/Character';
+import Error404 from '../page/Error404';
+
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
+
+
+const routes = {
+    '/': Home,
+    '/:id': Character,
+    '/contact': 'Contact',
+}
+
+// Manejador de rutas
+// Se encargara de mostrar los elementos
+const router = async () => {
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('content');
+    
+    header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    
+    content.innerHTML = await render();
+}
+export default router;
